2f36558c071362a10b22a1596dc3cc578cb72af3
Alberco <hyde2297@gmail.com>
2022-10-25 20:23:47 -0500

    uno

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ def suma(a, b):
     return a + b
 
 
+def resta(a, b):
+    return a - b
+
+
 def multiplicar(x, y):
     resultado = x + y
+    breakpoint()
     if resultado >= 4:
         return "resultado excelente"
     else:
         return None
-
-
-print(suma(2, 3))
